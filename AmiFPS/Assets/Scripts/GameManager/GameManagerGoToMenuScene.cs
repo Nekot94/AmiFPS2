92367966bbc59b3b1368aef7fcafc82111c2f5d9
NameNamovich <syntaxname@yandex.ru>
2017-08-23 14:19:08 +0300

    Add files via upload

---
+++ AmiFPS/Assets/Scripts/GameManager/GameManagerGoToMenuScene.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class GameManagerGoToMenuScene : MonoBehaviour
+{
+	public GameManagerMaster gameManagerMaster;
+
+	void OnEnable()
+	{
+		SetInitialReferences ();
+		gameManagerMaster.GoToMainMenuEvent += GoToMenuScene;
+	}
+
+	void OnDisable()
+	{
+		gameManagerMaster.GoToMainMenuEvent -= GoToMenuScene;
+	}
+
+	void SetInitialReferences()
+	{
+		gameManagerMaster = GetComponent<GameManagerMaster> ();
+	}
+
+	void GoToMenuScene()
+	{
+		SceneManager.LoadScene (0);
+	}
+}
